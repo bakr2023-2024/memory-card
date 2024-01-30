d578f963cdbc8bdadaba98fba63120467827da7a
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2024-01-31 01:54:38 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 const url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
-const getData = async (value) => {
+const getPokemon = async (value) => {
   try {
     const res = await fetch(url + value);
     const json = await res.json();
@@ -11,13 +11,18 @@ const getData = async (value) => {
     return { error: `${value} is not an id or name` };
   }
 };
-const data = [];
-for (let i = 1; i <= 16; i++) {
-  const datum = await getData(i);
-  data.push(datum);
-}
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App data={data} />
-  </React.StrictMode>
-);
+const setData = async () => {
+  const data = [];
+  for (let i = 1; i <= 16; i++) {
+    const datum = await getPokemon(i);
+    data.push(datum);
+  }
+  return data;
+};
+setData().then((data) => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App data={data} />
+    </React.StrictMode>
+  );
+});
